b3ee18c15e050e237528c997a424c1bd83052bc8
mile <791144831@qq.com>
2019-02-17 00:49:42 +0800

    登录-发送请求1

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="login-wrap">
-        <el-form 
-        class="login-form"
-        label-position="top" label-width="80px" :model="formdata">
+        <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
             <h2>用户名</h2>
             <el-form-item label="用户名">
                 <el-input v-model="formdata.username"></el-input>
@@ -10,24 +8,65 @@
             <el-form-item label="密码">
                 <el-input v-model="formdata.password"></el-input>
             </el-form-item>
-            <el-button class="login-btn" type="primary">登录</el-button>
+            <el-button @click.prevent="handlelogin()" class="login-btn" type="primary">登录</el-button>
         </el-form>
     </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            formdata:{
-                username:'',
-                password:''
-            }
-        };
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
     }
-};
+  },
+  methods: {
+    // 发起登录的请求
+    handlelogin () {
+      this.$http.post(`login`, this.formdata).then((res) => {
+        console.log(res)
+        const {
+          data: {
+            data,
+            meta: { msg, status }
+          }
+        } = res
+        if (status === 200) {
+          console.log('login--success--')
+        } else {
+          console.log('error---')
+        }
+        // const {per} = {per:"abc"}
+        // console.log(per)  abc
+        // const {name:newname} = {name:'abc'}
+        // console.log(newname)   abc
+      })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
+}
 </script>
 
 <style>
-
+.login-wrap {
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login-form {
+    background-color: #fff;
+    width: 400px;
+    border-radius: 5px;
+    padding: 30px;
+}
+.login-btn {
+    width: 100%;
+}
 </style>
